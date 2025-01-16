0ab1f4db4381bad1eca76462882f3bf7b6572aff
CrisOrtega <crisortegamunoz@gmail.com>
2025-01-15 21:58:11 -0300

    Se agregan comando para cipher y decipher

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { encode } from "punycode";
 import yargs, { alias, demand, demandOption, describe } from "yargs";
 import prng from "./prng";
+import cipher from "./cipher";
+import decipher from "./decipher";
 
 const { argv } = yargs
 .option({})
@@ -51,6 +53,76 @@ const { argv } = yargs
 
     }
   }
+})
+.command({
+  command: "cipher",
+  describe: "Encrypt a file",
+  handler: ({ password, salt, size, input, output }) => {
+    cipher(password, salt, size, input, output);
+  },
+  builder: {
+    password: {
+      alias: "p",
+      description: "The password to encrypt the file with",
+      type: "string",
+    },
+    salt: {
+      description: "The salt to encrypt the file with",
+      type: "string",
+    },
+    size: {
+      choices: [128, 192, 256] as const,
+      description: "The size of the key",
+      default: 128,
+    },
+    input: {
+      alias: 'i',
+      description: "The file to encrypt",
+      type: 'string',
+      demandOption: true
+    },
+    output: {
+      alias: 'o',
+      description: "The file to output the encrypted file to",
+      type: 'string',
+      demandOption: true
+    },
+  },
+})
+.command({
+  command: "decipher",
+  describe: "Decrypt a file",
+  handler: ({ password, salt, size, input, output }) => {
+    decipher(password, salt, size, input, output);
+  },
+  builder: {
+    password: {
+      alias: "p",
+      description: "The password to decrypt the file with",
+      type: "string",
+    },
+    salt: {
+      description: "The salt to decrypt the file with",
+      type: "string",
+    },
+    size: {
+      choices: [128, 192, 256] as const,
+      description: "The size of the key",
+      default: 128,
+    },
+    input: {
+      alias: 'i',
+      description: "The file to decrypt",
+      type: 'string',
+      demandOption: true
+    },
+    output: {
+      alias: 'o',
+      description: "The file to output the decrypted file to",
+      type: 'string',
+      demandOption: true
+    },
+  },
 })
 .demandCommand(1, 'You need at least one command before moving on')
 .help();
